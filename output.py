255108c2f4218e409797e97797fae1ac4259c422
raymundo21213 <157451536+raymundo21213@users.noreply.github.com>
2024-03-05 15:35:59 +0800

    first commit

---
+++ output.py
@@ -0,0 +1,13 @@
+#varivale declaration
+x = 25
+message = "Hi po!"
+ratio = 3.14
+
+#checking the type of data with type() method.
+print(type(message))
+print(type(ratio))
+print(type(x))
+
+#string formatting- f for fstring
+print(message + "my name is jessa. I am" + str(x) + "years old.")
+print(f"{message} my name is leslen. I am {x} years old.")
